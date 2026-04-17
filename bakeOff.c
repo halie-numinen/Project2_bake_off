9bed66c2ed68b75fe07ed51f05339117d7edc34f
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-04-17 19:23:14 -0400

    adding the logic for the threads and setting up for the semaphores.

--- bakeOff.c
+++ bakeOff.c
@@ -25,16 +25,31 @@ void handleSigint(int sig) { // helpes with garaceful shutdown
     }
 }
 
-struct sembuf p = { 0, -1, SEM_UNDO}; 
-struct sembuf v = { 0, +1, SEM_UNDO};
+struct sembuf acquire = { 0, -1, SEM_UNDO}; 
+struct sembuf release = { 0, +1, SEM_UNDO};
+
+int semId;
 
 int main () {
-    int semId, shmId;
+    int shmId;
     key_t key = IPC_PRIVATE;
     pthread_t thread1[500];  
     int threadStatus1; 
+    int *threadNumber;
+    
+    int bakers;
+    char *errorCheacking;
+    char* noBakers;
+    
+    printf("Please enter the number of bakers you want: ");
+    noBakers = fgets(numberOfBakersString, sizeof(numberOfBakersString), stdin);
+    if (noBakers != NULL && strlen(numberOfBakersString) > 1) {
+        exit(1);
+    }
+    numberOfBakersString[strcspn(numberOfBakersString, "\n")] = '\0';
+    bakers = (int)strtol(numberOfBakersString, &errorCheacking, 10);
+    signal(SIGINT, handleSigint);
 
-    int numberOfBakers = 0; 
     if ((shmId = shmget(key, 1024, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
         perror("Unable to get shared memory\n");
         exit(1);
@@ -43,7 +58,6 @@ int main () {
         perror("Unable to get semaphore\n");
         exit(1);
     }
-    signal(SIGINT, handleSigint);
     if ((sharedMemoryPtr = (struct resource *) shmat(shmId, 0, 0)) == (void*) -1) { // add in the main struct here (mixer, pantry, spoon, ect.)
         perror("Unable to attach\n");
         exit(1);
@@ -53,5 +67,29 @@ int main () {
         exit(1);
     }
 
+    for (int i = 0; i < bakers; i++) {
+        threadNumber = malloc(sizeof(int));
+        if (!threadNumber) {
+            perror("malloc failed.");
+            exit(1);
+        } 
+
+        threadStatus1 = pthread_create(&thread1[i], NULL,  bakerActivities, &bakers); // what is the last number (pointer to the number of bakers)
+        if (threadStatus1 != 0){ 
+            fprintf (stderr, "Thread create error %d: %s\n", threadStatus1, strerror(threadStatus1)); 
+            free(threadNumber);
+            continue;
+        } 
+    }
+    for(int i = 0; i < bakers; i++) {
+        pthread_join(thread1[i], NULL); 
+    } 
+    free(kitchen);
+    free(threadNumber);
     return 0;
+}
+
+void* bakerActivities(void* arg) {
+    // for the semaphores and locking and unlocking resources
+    return NULL;
 }
