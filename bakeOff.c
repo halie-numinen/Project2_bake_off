bd27b0586efe7e45507861fcc68cbe21a308f5dc
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-04-20 22:05:42 -0400

    adding the ending logic and somethings for the backeractivities function.

--- bakeOff.c
+++ bakeOff.c
@@ -43,7 +43,7 @@ int main () {
     
     printf("Please enter the number of bakers you want: ");
     noBakers = fgets(numberOfBakersString, sizeof(numberOfBakersString), stdin);
-    if (noBakers != NULL && strlen(numberOfBakersString) > 1) {
+    if (noBakers != NULL && strlen(numberOfBakersString) < 1) {
         exit(1);
     }
     numberOfBakersString[strcspn(numberOfBakersString, "\n")] = '\0';
@@ -54,7 +54,7 @@ int main () {
         perror("Unable to get shared memory\n");
         exit(1);
     }
-    if ((semId = semget(IPC_PRIVATE, 1, S_IRUSR | S_IWUSR)) == -1) { 
+    if ((semId = semget(IPC_PRIVATE, 6, S_IRUSR | S_IWUSR)) == -1) { 
         perror("Unable to get semaphore\n");
         exit(1);
     }
@@ -62,9 +62,12 @@ int main () {
         perror("Unable to attach\n");
         exit(1);
     }
-    if (semctl(semId, 0, SETVAL, 1) == -1) {
-        perror("unable to initialize semaphore\n");
-        exit(1);
+    int maxAmounts[] = {1, 2, 2, 3, 5, 1}; // pantries, friges, mixers, bowls, spoons, ovens
+    for (int i = 0; i < 6; i++) {
+        if (semctl(semId, i, SETVAL, maxAmounts[i]) == -1) {
+            perror("unable to initialize semaphore\n");
+            exit(1);
+        }
     }
 
     for (int i = 0; i < bakers; i++) {
@@ -74,7 +77,7 @@ int main () {
             exit(1);
         } 
 
-        threadStatus1 = pthread_create(&thread1[i], NULL,  bakerActivities, &bakers); // what is the last number (pointer to the number of bakers)
+        threadStatus1 = pthread_create(&thread1[i], NULL,  bakerActivities, threadNumber); // what is the last number (pointer to the number of bakers)
         if (threadStatus1 != 0){ 
             fprintf (stderr, "Thread create error %d: %s\n", threadStatus1, strerror(threadStatus1)); 
             free(threadNumber);
@@ -84,12 +87,26 @@ int main () {
     for(int i = 0; i < bakers; i++) {
         pthread_join(thread1[i], NULL); 
     } 
-    free(kitchen);
-    free(threadNumber);
+    if (shmdt(kitchen) < 0) { // the shared memory pointer
+        perror("Unable to detach shared memory\n");
+        exit(1);
+    }
+    if (semctl(semId, 0, IPC_RMID) == -1) {
+        perror("Unable to deallocate semophore\n");
+        exit(1);
+    }
+    if (shmctl(shmId, IPC_RMID, 0) < 0) {
+        perror("Unable to deallocate shared memory\n");
+        exit(0);
+    }
     return 0;
 }
 
-void* bakerActivities(void* arg) {
+void* bakerActivities(void* bakerId) {
     // for the semaphores and locking and unlocking resources
+    int numberOfRecipies = 5;
+    char *colors[] = {"red", "gold", "green", "blue", "yellow", "orange", "purple", "pink", "white", "black", "brown", "silver"};
+    int id = *((int*)bakerId);
+    free(bakerId);
     return NULL;
 }
